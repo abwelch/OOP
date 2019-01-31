a8a968a898ef78527414a6ab516636d2d04497eb
Andrew Welch <46785095+abwelch@users.noreply.github.com>
2019-01-30 21:06:33 -0500

    Delete warGame.cpp

--- war/warGame.cpp
+++
@@ -1,9 +0,0 @@
-#include "deck.hpp"
-#include "card.hpp"
-
-// Default constructor to generate a randomly sorted deck and distribute to both
-// player vectors
-deck::deck()
-{
-    card unsortedDeck[52];
-}
