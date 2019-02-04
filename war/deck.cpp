df0b5a8bc1ad59ae56183fd4c5f85a65bb8831a5
Drew Welch <Drew@Drews-iMac.local>
2019-02-04 09:52:38 -0500

    built default constructor for deck

--- war/deck.cpp
+++ war/deck.cpp
@@ -1,19 +1,42 @@
-#include "warGame.hpp"
+#include "deck.hpp"
 #include "card.hpp"
 
 // Default constructor to generate a randomly sorted deck and distribute to both
 // player vectors
-warGame::warGame()
+deck::deck()
 {
     // Creates an array to temporarily hold all cards for random assignment
     card fullDeck[52];
     // Assign all card combinations to random array positions
     unsigned seed = time(0);
     srand(seed);
+    int usedPosition[52];
+    bool inUse = false;
+    int randomPosition;
+    const int MIN = 0, MAX = 51;
+
     for (int i = 0; i < 4; ++i)
     {
         for (int j = 0; j < 13; ++j)
         {
+            do
+            {
+                randomPosition = (rand() % (MAX - MIN + 1)) + MIN;
+                for (int i = 0; i < 52; ++i)
+                {
+                    if (randomPosition == usedPosition[i])
+                    {
+                        inUse = true;
+                    }
+
+                    else
+                    {
+                        inUse = false;
+                    }
+                }
+            } while (inUse);
+            card newCard(static_cast<rank>(j), static_cast<suit>(i));
+            fullDeck[randomPosition] = newCard;
         }
     }
 }
