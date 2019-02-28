ce689b91acda3c0eaa024618e5f85069d5fe5f80
Drew Welch <Drew@Drews-iMac.local>
2019-02-28 15:55:57 -0500

    Implemented block constructor

--- Arkanoid (Project)/Block.cpp
+++ Arkanoid (Project)/Block.cpp
@@ -1,1 +1,14 @@
 #include "Block.hpp"
+
+Block::Block(int size)
+{
+    blockSprites.reserve(size);
+    int n = 0;
+    for (int i = 1; i <= 10; i++)
+        for (int j = 1; j <= 10; j++)
+        {
+            blockSprites[n].setTexture(blockTexture.getBlockTexture());
+            blockSprites[n].setPosition(i * 43, j * 20);
+            n++;
+        }
+}
--- Arkanoid (Project)/Block.hpp
+++ Arkanoid (Project)/Block.hpp
@@ -2,5 +2,15 @@
 #define BLOCK_HPP
 
 #include "Textures.hpp"
+#include <vector>
 
+class Block
+{
+  private:
+    Textures blockTexture;
+    std::vector<Sprite> blockSprites;
+
+  public:
+    Block(int);
+};
 #endif
