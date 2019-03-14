62cfd1bad93783dd1d10e63ac9f6e8db7e11542d
Drew Welch <Drew@Drews-iMac.local>
2019-03-14 19:33:59 -0400

    Added enums and card class framework

--- HierarchicalPlayingCards (Practice 5)/card.hpp
+++ HierarchicalPlayingCards (Practice 5)/card.hpp
@@ -1,6 +1,41 @@
-#ifndef
-#define
+/*
+Card class that utilizes an enum for determining if a playing card will be
+a joker card or a standard card. Joker and standard cards are derived from the
+card class via inheritance
+*/
 
+#ifndef CARD_HPP
+#define CARD_HPP
 
+#include <cassert>
+#include <deque>
 
+enum Suit {
+  TWO,
+  THREE,
+  FOUR,
+  FIVE,
+  SIX,
+  SEVEN,
+  EIGHT,
+  NINE,
+  TEN,
+  JOKER,
+  QUEEN,
+  KING,
+  ACE,
+};
+
+enum Color {
+  Red,
+  Black,
+};
+
+class Card {
+public:
+  enum Type {
+    JOKER,
+    STANDARD,
+  };
+};
 #endif
