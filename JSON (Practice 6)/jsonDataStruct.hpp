ca95e8512213eac5e5e99d1470d1ec71cf19cedf
Drew Welch <Drew@Drews-iMac.local>
2019-04-02 23:01:28 -0400

    Create jsonDataStruct outline

---
+++ JSON (Practice 6)/jsonDataStruct.hpp
@@ -0,0 +1,15 @@
+#ifndef JSONDATASTRUCT_HPP
+#include JSONDATASTRUCT_HPP
+
+#include <string>
+
+class JsonDataStruct
+{
+private:
+	std::string author;
+	std::string title;
+public:
+	
+}
+
+#endif
