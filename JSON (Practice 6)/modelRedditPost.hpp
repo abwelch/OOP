20b0482ca047f70b204879bf0a1c3624fbb03401
Drew Welch <Drew@Drews-iMac.local>
2019-04-02 23:05:55 -0400

    Create model reddit post outline

---
+++ JSON (Practice 6)/modelRedditPost.hpp
@@ -0,0 +1,15 @@
+#ifndef MODELREDDITPOST_HPP
+#include MODELREDDITPOST_HPP
+
+#include <string>
+
+class ModelRedditPost
+{
+private:
+	std::string author;
+	std::string title;
+public:
+	
+}
+
+#endif
