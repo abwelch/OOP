0cc0b1d0a70d4b0cafe8681e5b60137ef883f921
Andrew Welch <abw28@zips.uakron.edu>
2019-05-03 18:52:48 -0400

    Alter main to utilize event structs

--- Arkanoid (Project)/main.cpp
+++ Arkanoid (Project)/main.cpp
@@ -16,11 +16,39 @@ struct Event_listener {
 struct Event_source {
   template <typename T> void notify(T funct) {
     for (Event_listener *left : listeners)
-      funct(left);
+      ++left;
   }
   sf::Window *window;
   std::vector<Event_listener *> listeners;
   Event_source(sf::Window &w) : window(&w) {}
+  void listen(Event_listener &left) { listeners.push_back(&left); }
+  void process(sf::Event const &e) {
+    switch (e.type) {
+    case sf::Event::KeyPressed:
+      return notify(e);
+    case sf::Event::KeyReleased:
+      return notify(e);
+    default:
+      break;
+    }
+  }
+  void poll() {
+    sf::Event e;
+    while (window->pollEvent(e)) {
+      process(e);
+    }
+  }
+};
+
+struct arkanoid_app : Event_listener {
+  sf::RenderWindow window;
+  arkanoid_app() : window(sf::VideoMode(520, 450), "Arkanoid!") {
+    window.setFramerateLimit(60);
+  }
+  bool is_open() const { return window.isOpen(); }
+
+  void on_close() override { window.close(); }
+  void draw(Instance &object, Textures texts) { window.clear(); }
 };
 
 int main() {
@@ -29,5 +57,10 @@ int main() {
   Instance arkGame(images);
   arkGame.difficultySetting();
   arkGame.startGame(images);
+  arkanoid_app app;
+  Event_listener listening;
+  sf::Window window;
+  Event_source events(app.window);
+  events.listen(app);
   return 0;
 }
