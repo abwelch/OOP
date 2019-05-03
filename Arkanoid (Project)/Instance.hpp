3cbd55757bfbf1492e6545bbd71391ecb5289aad
Drew Welch <Drew@Drews-iMac.local>
2019-05-03 13:24:17 -0400

    Change abstraction method

--- Arkanoid (Project)/Instance.hpp
+++ Arkanoid (Project)/Instance.hpp
@@ -1,20 +1,19 @@
-/* 
+/*
 Executes necessary functions to run an instance of Arkanoid
 */
 
 #ifndef INSTANCE_HPP
 #define INSTANCE_HPP
 
+#include "Background.hpp"
 #include "Ball.hpp"
 #include "Blocky.hpp"
-#include "Background.hpp"
 #include "Paddle.hpp"
 #include "Textures.hpp"
 
 #include <time.h>
 
-class Instance
-{
+class Instance {
 private:
   float x, y, dx, dy;
   int n;
@@ -25,12 +24,11 @@ private:
   int score, difficulty;
 
 public:
-  Instance(Textures &images) : x(300), y(300), dx(6), dy(5), n(0),
-                               blocks(n, images.blockImage, images.redBlockImage), ball(images.ballImage),
-                               background(images.backgroundImage), paddle(images.padImage),
-                               score(0), difficulty(0)
-  {
-  }
+  Instance(Textures &images)
+      : x(300), y(300), dx(6), dy(5), n(0),
+        blocks(n, images.blockImage, images.redBlockImage),
+        ball(images.ballImage), background(images.backgroundImage),
+        paddle(images.padImage), score(0), difficulty(0) {}
   void difficultySetting();
   void startGame(const Textures &);
   void drawObjects(sf::RenderWindow &);
